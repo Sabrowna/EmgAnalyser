329422ad6f9cd2e28de82a230a80d08a47556fb8
Sabrowna <201906541@post.au.dk>
2022-10-28 13:27:37 +0200

    convertGripToActions method missing to return dto

--- EmgTransformer.py
+++ EmgTransformer.py
@@ -1,8 +1,13 @@
+import configparser
+from nntplib import GroupInfo
 import EmgReader
 
 
 class EmgTransformer():
     newActionReceived = False
+
+    def __init__(self) -> None:
+        self.adc = EmgReader.Adc()
 
     def openGrip(self):
         self.newActionReceived = True
@@ -26,3 +31,63 @@ class EmgTransformer():
                     return self.closeGrip()
 
         # Move to convertToAction()?
+
+    def convertGripToActions(self, voltage):
+        # 1: loop through sensors
+        # 2: for each sensor:
+        # - How many motors controlling out of 5?
+        # - Grip type? open or closing grip?
+        # ----------- MISSING ------------
+        # Return ActionDTO
+        # Check for voltage - if it is +2, startGrip else stopMotor
+        # Which sensor has received something?
+
+        # Reading the file by using a parser
+        configParser = configparser.RawConfigParser()
+        configFilePath = r"C:\Users\Bruger\Documents\7. semester\Bachelor\SW\EmgAnalyser\config.txt"
+        configParser.read(configFilePath)
+
+        # list of actions
+        actionlist = []
+
+        # looping through each line in the [sensors] section (all sensors)
+        for (sensor, motorControlling) in configParser.items('sensors'):
+            print(sensor + ': ' + motorControlling)
+            motorlist = motorControlling
+            gripType = motorlist[len(motorlist) - 1]
+
+            actionlist.clear()
+
+            # looping through 5 times, one for each motor
+            for i in range(1, 6):
+                if (int(motorlist[i-1]) == i):
+                    actionlist.append(gripType)
+                else:
+                    actionlist.append('na')
+
+            print('actionlist: ' + str(actionlist))
+
+
+e = EmgTransformer()
+e.convertGripToActions(2)
+
+
+# read config file
+# read all adcs
+# if the value from adc is higher than 2
+# which sensor are we talking about?
+# Is it an opening or closing action?
+# How many motors?
+# Put it into an action form
+
+
+def readConfigFile():
+    return 'configObj'
+
+
+def readSensor(self, sensorNum):
+    return 'grip'
+
+
+def processEmgSignal(grip):
+    return 'dto action'
