1e338c2cdf5ecb5602085362455814378e21b098
Sabrowna <201906541@post.au.dk>
2022-11-04 17:44:08 +0100

    no sensor, open, close, under and over 2v running

--- Test_EmgTransformer.py
+++ Test_EmgTransformer.py
@@ -29,30 +29,136 @@ class TransformerTest(unittest.TestCase):
         dto.actions[4] = enum.stop
 
         # Assert
-        print(result)
-        print(dto.actions)
         self.assertEqual(result, dto.actions)
 
-        # Når jeg tester metoderne, er det ikke nødvendigt at teste de private metoder. De er kun til for at gøre koden mere overskuelig.
-        # Assert på hvad du forventer at få tilbage når du tester metoden.
-        # Ved denne metode skal der returneres noget, alt efter hvad ADC'en sender tilbage.
-        # For at kunne styre adc'en er det derfor nødvendigt med en fake adc.
-        # Evt. også en fake config file, der sendes ind gennem dependency injection, sådan at man kan have forskellige filer med forskellige antal sensorer osv.
+    def test_handleSensorValues_sendingClose0sensors_defaultAction(self):
+        # What happens when no sensors is received from config
 
-        # Mangler at teste: 1, 3, 4 sensorer
+        # Arrange
+        configPath = '0sensors.ini'
+        sensorValues = {0: 0.01, 1: 2.53}
+        enum = ActionEnum
+        dto = DTO_Action()
+
+        emgReader = FakeAdc(sensorValues)
+        transformer = EmgTransformer.EmgTransformer(
+            'pair', emgReader, configPath)
+
+        # Act
+        result = transformer.handleSensorValues()
+        dto.actions[0] = enum.stop
+        dto.actions[1] = enum.stop
+        dto.actions[2] = enum.stop
+        dto.actions[3] = enum.stop
+        dto.actions[4] = enum.stop
+
+        # Assert
+        self.assertEqual(result, dto.actions)
+
+    def test_handleSensorValues_sendingValuesUnder2V_defaultAction(self):
+        # What happens when no sensors is received from config
+
+        # Arrange
+        configPath = '2sensors.ini'
+        sensorValues = {0: 0.01, 1: 1.53}
+        enum = ActionEnum
+        dto = DTO_Action()
+
+        emgReader = FakeAdc(sensorValues)
+        transformer = EmgTransformer.EmgTransformer(
+            'pair', emgReader, configPath)
+
+        # Act
+        result = transformer.handleSensorValues()
+        dto.actions[0] = enum.stop
+        dto.actions[1] = enum.stop
+        dto.actions[2] = enum.stop
+        dto.actions[3] = enum.stop
+        dto.actions[4] = enum.stop
+
+        # Assert
+        self.assertEqual(result, dto.actions)
+
+    def test_handleSensorValues_sendingValuesOver2V_defaultAction(self):
+        # What happens when no sensors is received from config
+
+        # Arrange
+        configPath = '2sensors.ini'
+        sensorValues = {0: 2.01, 1: 2.53}
+        enum = ActionEnum
+        dto = DTO_Action()
+
+        emgReader = FakeAdc(sensorValues)
+        transformer = EmgTransformer.EmgTransformer(
+            'pair', emgReader, configPath)
+
+        # Act
+        result = transformer.handleSensorValues()
+        dto.actions[0] = enum.stop
+        dto.actions[1] = enum.stop
+        dto.actions[2] = enum.stop
+        dto.actions[3] = enum.stop
+        dto.actions[4] = enum.stop
+
+        # Assert
+        self.assertEqual(result, dto.actions)
+
+    def test_handleSensorValues_sendingOpenValues_Open(self):
+        # What happens when no sensors is received from config
+
+        # Arrange
+        configPath = '2sensors.ini'
+        sensorValues = {0: 2.01, 1: 0.53}
+        enum = ActionEnum
+        dto = DTO_Action()
+
+        emgReader = FakeAdc(sensorValues)
+        transformer = EmgTransformer.EmgTransformer(
+            'pair', emgReader, configPath)
+
+        # Act
+        result = transformer.handleSensorValues()
+        dto.actions[0] = enum.open
+        dto.actions[1] = enum.open
+        dto.actions[2] = enum.open
+        dto.actions[3] = enum.stop
+        dto.actions[4] = enum.stop
+
+        # Assert
+        self.assertEqual(result, dto.actions)
+
+    # def test_handleSensorValues_4sensors_OpenOpen(self):
+    #     # What happens when no sensors is received from config
+
+    #     # Arrange
+    #     configPath = '4sensors.ini'
+    #     sensorValues = {0: 2.01, 1: 0.53, 2: 2.01, 3: 0.53}
+    #     enum = ActionEnum
+    #     dto = DTO_Action()
+
+    #     emgReader = FakeAdc(sensorValues)
+    #     transformer = EmgTransformer.EmgTransformer(
+    #         'pair', emgReader, configPath)
+
+    #     # Act
+    #     dto.actions.clear()
+    #     result = transformer.handleSensorValues()
+    #     dto.actions[0] = enum.open
+    #     dto.actions[1] = enum.open
+    #     dto.actions[2] = enum.open
+    #     dto.actions[3] = enum.open
+    #     dto.actions[4] = enum.open
+
+    #     # Assert
+    #     self.assertEqual(result, dto.actions)
+
         # open, close metoder
         # not applicable
-        # ingen sensorer
         # for mange sensorer?
-        # begge sensorer under 2V
-        # begge sensorer +2V
-
         # ------------------ Andre tests ----------------
         # sender kun når der modtages nyt greb
         # Errors
         # serialiserer korrekt
         # error handling on uneven sensornumbers
-
-
 if __name__ == '__main__':
     unittest.main()
